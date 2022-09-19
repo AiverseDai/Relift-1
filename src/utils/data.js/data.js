d36b0487b23ab59059b929df9467fcc601fcf201
mohit-kumar9 <113946038+mohit-kumar9@users.noreply.github.com>
2022-09-20 03:38:22 +0530

    Add files via upload

---
+++ src/utils/data.js/data.js
@@ -0,0 +1,80 @@
+import image1 from "../../assets/images/image1.png";
+import image2 from "../../assets/images/image2.png";
+import image3 from "../../assets/images/image3.png";
+import image4 from "../../assets/images/image4.png";
+import image5 from "../../assets/images/image5.png";
+import fb from "../../assets/images/fb.png";
+import insta from "../../assets/images/instagram.png";
+import whats from "../../assets/images/whatsapp.png";
+import twitter from "../../assets/images/twitter.png";
+import image11 from "../../assets/images/image11.png";
+import image12 from "../../assets/images/image12.png";
+// import { BsQuestionCircleFill } from "react-icons/bs";
+
+export const disasterList = [
+  {
+    id: 1,
+    name: "Tornado",
+    img: image1,
+  },
+  {
+    id: 2,
+    name: "Flood",
+    img: image2,
+  },
+  {
+    id: 3,
+    name: "Wildfire",
+    img: image3,
+  },
+  {
+    id: 4,
+    name: "Earthquakes",
+    img: image4,
+  },
+  {
+    id: 5,
+    name: "Tsunami",
+    img: image5,
+  },
+  {
+    id: 6,
+    name: "Others",
+    img: "https://images.pexels.com/photos/3683107/pexels-photo-3683107.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  },
+];
+
+export const icons = [
+  {
+    id: 1,
+    name: "facebook",
+    icon: fb,
+  },
+  {
+    id: 2,
+    name: "whatsapp",
+    icon: whats,
+  },
+  {
+    id: 3,
+    name: "instagram",
+    icon: insta,
+  },
+  {
+    id: 4,
+    name: "twitter",
+    icon: twitter,
+  },
+];
+export const linkImages = [
+  {
+    id: 1,
+    name: "ios",
+    img: image11,
+  },
+  {
+    id: 2,
+    name: "android",
+    img: image12,
+  },
+];
